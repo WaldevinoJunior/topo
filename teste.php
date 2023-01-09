f92369bc18df4a428b3c4a17e7bfae3dd4d44898
Gui20Henriques <guilherwme20@protonmail.com>
2023-01-09 19:06:23 -0300

    Alterações no teste.php

--- teste.php
+++ teste.php
@@ -7,11 +7,31 @@
     <head><meta charset="utf8"> 
     <link rel="stylesheet" href="css/loginNovo.css" type="text/css">
     <script src="js/altera.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+    <link rel="sortcut icon" href="img/iconetopo.jpg" type="image/jpg" />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/loginNovo.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="js/constroi.js"> </script>
     </head>
     <body>
-        <section>
-        <h2 id="oi">Aluno</h2>
-        <form id="form"  method="POST" action="valida.php">
+    <nav class="menuTeste">
+        <a href="./usuario.php" ><img src="img/iconetopo.jpg"></a>
+    </nav>
+        <section  id="formTeste">
+        <h2 id="oi"><?php if(!isset($_SESSION)){session_start();} echo $_SESSION['nome']; ?></h2>
+        <form  id="formTeste" method="POST" action="valida.php">
             <fieldset id="field">
         <?php
             $arq= "cursos/".$_GET['idcurso']."/".$_GET['i2']."/teste.txt";
@@ -33,7 +53,7 @@
                     if($i4 > 2 && $i4<7){
                         $i5++;
                         $linha = str_replace(";;", "", $linha);
-                        echo "<input type='checkbox' name='pergunta".$i6."'value='".$i5."'>".$linha."</br>";
+                        echo "<input type='checkbox' class='checkmark 'name='pergunta".$i6."'value='".$i5."'>".$linha."</br>";
                     }
                     if($i4 == 7){
                         $i4 = 0;
@@ -54,13 +74,13 @@
                 unset($_COOKIE['total']);
             }
         ?>
-        <a onclick="validar()">mostrar resultado</a>
+        <a onclick="validar()" class="btn btn-success btn-sm" id="mostraResultado">Mostrar resultado</a>
         </fieldset>
         <div id="nota" style="display:none;">
-            <h1>nota:</h1><h1 id="resul"> oiii</h1>
-            <p id="result2"></p>
+            <h2>nota:</h2><h1 id="resul">nota</h1>
+            <h1 id="result2">nota</h1>
         </div>
-        <input type="submit" value="Enviar" name="enviarteste"  id="enviar" style="display:none">
+        <input type="submit" value="Enviar" name="enviarteste"  class="btn btn-success btn-sm" id="enviar" style="display:none">
         </form>
         
         <script type="text/javascript"> 
@@ -96,10 +116,22 @@
                 document.getElementById("resul").innerHTML = total;
                 document.getElementById("enviar").style.display = "block";
                 if(total>70){
-                    document.getElementById("result2").innerHTML = "APROVADO";
+                    let re = document.getElementById("result2");
+                    let imagem = document.createElement("img");
+                    imagem.setAttribute("src", "/topo/img/aprovado.png");
+                    imagem.setAttribute("id", "imgResul");
+                    re.innerHTML = "APROVADO";
+                    re.setAttribute("id", "aprov");
+                    document.getElementById("nota").appendChild(imagem);
                 }
                 if(total<70){
-                    document.getElementById("result2").innerHTML = "REPROVADO";
+                    let re = document.getElementById("result2");
+                    let imagem = document.createElement("img");
+                    imagem.setAttribute("src", "/topo/img/reprovado.png");
+                    imagem.setAttribute("id", "imgResul");
+                    re.innerHTML = "REPROVADO";
+                    re.setAttribute("id", "reprov");
+                    document.getElementById("nota").appendChild(imagem);
                 }
                 document.cookie = "total="+total+"";
             }
