2f799c4eb0cd8f4416994b8965d6a4d515951164
Gui20Henriques <guilherwme20@protonmail.com>
2023-01-19 16:44:17 -0300

    Alterei o menu do admin

--- admin.php
+++ admin.php
@@ -37,7 +37,7 @@
 <body id="adminBody">
     <nav class="menuAdmin">
         <img src="img/iconetopo.jpg">
-        <ul>
+        <ul id="mAdmin">
             <li><a href="./admin.php"><i class="bi bi-house"></i>Início</a></li>
             <li><a href=""><i class="bi bi-person-badge"></i>Cadastro<i class="bi bi-caret-down"></i></a>
                     <div id="barras">
@@ -70,179 +70,13 @@
 
             <li><a href="./valida.php?sair=true"><i class="bi bi-escape"></i>Sair</a></li>
         </ul>
+        <img id="mAdmin2" src="img/apertomao.jpg"/>
     </nav>
         <div class="admin">
             <div id="painel">
                 <h2><strong>Administração</strong></h2>
                 <!-- <a href="admin.php" class="btn btn-primary btn-sm">Voltar</a> -->
             <div id="func">
-                <div id="listaAlunos" style="display:none" class="listAlunos">
-                <div class="cont-header" id="cbcLista">
-                    <h1>Lista de alunos</h1>
-                    <form action="buscarAluno.php" method="POST">
-                    <select name='aluno'>
-                        <?php
-                         while($cAlunos = mysqli_fetch_array($conAlunos2)){
-                            echo "<option id='busca' value='".$cAlunos['ID_Aluno']."'>".$cAlunos['Nome']." - ".$cAlunos['CPF']."</option>";
-                        }
-                        ?>
-                    </select>
-                    <input type="submit" class="btn btn-success btn-sm" style='background-color:blue;margin-top:10px;font-size:15px' name="buscaAluno" value='Buscar'></input>
-                    </form>
-                    <br><a onclick="voltaAdmin()" class="btn btn-success btn-sm" style="background-color:blue;margin-top:10px">Voltar</a>
-                </div>
-
-                <div class="content" style="overflow-y: scroll;height:500px">   
-                    <?php
-                       $table = '<table class="table table-striped" id="tableAluno">';
-                            $table .='<thead>';
-                                $table .= '<tr>';
-                                   $table .= '<th>ID</th>';
-                                   $table .= '<th>Nome</th>';
-                                //    $table .= '<th>Responsável</th>';
-                                   $table .= '<th>Nascimento</th>';
-                                   $table .= '<th>Email</th>';
-                                   $table .= '<th>Telefone</th>';
-                                //    $table .= '<th>CPF</th>';
-                                //    $table .= '<th>RG</th>';
-                                //    $table .= '<th>CEP</th>';
-                                //    $table .= '<th>Estado</th>';
-                                //    $table .= '<th>Cidade</th>';
-                                //    $table .= '<th>Rua</th>';
-                                //    $table .= '<th>Número</th>';
-                                //    $table .= '<th>Senha</th>';
-                                $table .= '<th>Funções</th>';
-                                $table .= '<th>Curso</th>';
-                                $table .= '</tr>';
-                            $table .= '</thead>';
-                            $table .= '<tbody>';
-           
-                                while($cAlunos = mysqli_fetch_array($conAlunos)){
-                                    $table .= "<tr class='alunoBusca'  name=".$cAlunos['ID_Aluno'].">";
-                                        $table .= "<td>{$cAlunos['ID_Aluno']}</td>";
-                                        $table .= "<td>{$cAlunos['Nome']}</td>";
-                                        // $table .= "<td>{$cAlunos['Responsavel']}</td>";
-                                        $table .= "<td>{$cAlunos['Nascimento']}</td>";
-                                        $table .= "<td>{$cAlunos['Email']}</td>";
-                                        
-                                         $table .= "<td>{$cAlunos['Telefone']}</td>";
-                                        // $table .= "<td>{$cAlunos['CPF']}</td>";
-                                        // $table .= "<td>{$cAlunos['RG']}</td>";
-                                        // $table .= "<td>{$cAlunos['CEP']}</td>";
-                                        // $table .= "<td>{$cAlunos['Estado']}</td>";
-                                        // $table .= "<td>{$cAlunos['Cidade']}</td>";
-                                        // $table .= "<td>{$cAlunos['Rua']}</td>";
-                                        // $table .= "<td>{$cAlunos['Numero']}</td>";
-                                        // $table .= "<td>{$cAlunos['Senha']}</td>";
-                                        $table .= "<td><button onclick='editarAlunos".$cAlunos['ID_Aluno']."();'style = 'margin:10px;'class='btn btn-primary btn-sm'value='".$cAlunos['ID_Aluno']."'>Editar</button></td>";
-                                        $table .= "<td><a style='background-color:green;border:1px solid black;color:white;font-size:15px;margin-top:9px;padding:2.2px' href='cursoAluno.php?alunoid=".$cAlunos['ID_Aluno']."&&nome=".$cAlunos['Nome']."' style = 'margin:10px;font-size:15px;' 'value='".$cAlunos['ID_Aluno']."'>Adicionar</a></td>";
-                                        $table .= '</tr></div>';
-                                    
-                                    echo "<script>
-                                            function editarAlunos".$cAlunos['ID_Aluno']."(){
-    
-                                                document.getElementById('EditarAlunos".$cAlunos['ID_Aluno']."').style.display = 'block';
-                                                document.getElementById('tableAluno').style.display = 'none';;
-                                                document.getElementById('cbcLista').style.display = 'none';;
-                                                // alert(value);
-                                             
-                                            }
-                                        </script>";
-                                       
-                                        echo "<div id='EditarAlunos".$cAlunos['ID_Aluno']."' style='display:none;'>
-                                        <div id= 'dadosDoAluno' style='padding:10px;'>
-                                        <h3>Dados do aluno</h3>
-                                        <p>Preencha somente os dados que você quiser alterar</p>
-                                        <form method='post' action='valida.php'>
-                                            <input type='hidden' name='_token' value='WmrC6gcNsjkmzVGYVTc9EemXmdDXh5Zavb5ywoMY'>
-                                            <div class='row'>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='nome'>Nome</label>
-                                                    <input type='text' class='form-control' id='nome".$cAlunos['ID_Aluno']."' name='nome'  value ='".$cAlunos['Nome']."''>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='nome'>Responsavel</label>
-                                                    <input type='text' class='form-control' id='responsavel".$cAlunos['ID_Aluno']."' name='responsavel'  value ='".$cAlunos['Responsavel']."''>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='nasc'>Data de Nasc.</label>
-                                                    <input type='date' class='form-control' id='nascimento".$cAlunos['ID_Aluno']."' name='nascimento' value ='".$cAlunos['Nascimento']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='email'>Email</label>
-                                                    <input type='email' class='form-control' id='email".$cAlunos['ID_Aluno']."' name='email' value ='".$cAlunos['Email']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='tel'>Telefone</label>
-                                                    <input type='text' class='form-control' id='tel".$cAlunos['ID_Aluno']."' name='telefone' value ='".$cAlunos['Telefone']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='cpf'>CPF</label>
-                                                    <input type='text' class='form-control' id='cpf".$cAlunos['ID_Aluno']."' name='cpf' value ='".$cAlunos['CPF']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='rg'>RG</label>
-                                                    <input type='text' class='form-control' id='rg".$cAlunos['ID_Aluno']."' name='rg' value ='".$cAlunos['RG']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='cep'>CEP</label>
-                                                    <input type='text' class='form-control' id='cep".$cAlunos['ID_Aluno']."' name='cep' value ='".$cAlunos['CEP']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='estado'>Estado</label>
-                                                    <input type='text' class='form-control' id='estado".$cAlunos['ID_Aluno']."' name='estado' value ='".$cAlunos['Estado']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='cidade'>Cidade</label>
-                                                    <input type='text' class='form-control' id='cidade".$cAlunos['ID_Aluno']."' name='cidade' value ='".$cAlunos['Cidade']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='rua'>Rua</label>
-                                                    <input type='text' class='form-control' id='rua".$cAlunos['ID_Aluno']."' name='rua'value ='".$cAlunos['Rua']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='numero'>Número</label>
-                                                    <input type='text' class='form-control' id='numero".$cAlunos['ID_Aluno']."' name='numero' value ='".$cAlunos['Numero']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='complemento'>Complemento</label>
-                                                    <input type='text' class='form-control' id='complemento".$cAlunos['ID_Aluno']."' name='complemento' value ='".$cAlunos['Complemento']."'>
-                                                </div>
-                                                <div class='form-group col-12 col-lg-6'>
-                                                    <label for='senha'>Senha</label>
-                                                    <input type='text' class='form-control' id='senha".$cAlunos['ID_Aluno']."' name='senha' value ='".$cAlunos['Senha']."'>
-                                                </div>
-                                                <input type='text' name='id' style='display:none;' value='".$cAlunos['ID_Aluno']."'>
-                                            </div>
-            
-                           
-                                            <div class='d-flex justify-content-center'>
-                                            <a onclick='volta".$cAlunos['ID_Aluno']."()' class='btn btn-success mr-2' style='background-color:blue;width:70px;height:40px;font-size:16px;color:white;'>Voltar</a>
-                                            <script>
-                                            function volta".$cAlunos['ID_Aluno']."(){
-                                                window.scrollTo(0, 0);
-                                                document.getElementById('EditarAlunos".$cAlunos['ID_Aluno']."').style.display = 'none';
-                                                document.getElementById('tableAluno').style.display = 'block';
-                                                document.getElementById('cbcLista').style.display = 'block';
-                                                
-                                            }
-                                            </script>
-
-                                                <input class='btn btn-success mr-2'type='submit' value='Enviar' name='enviareditarAluno'>
-                                            </div>
-                                        </form>
-                                    </div>     
-                                </div>";
-
-                            } 
-                        $table .= '</tbody>';
-                        $table .= '</table>';
-                        echo $table;
-                   ?>
-                </div>
-                </div>
-                 
-                
                 <div id="listaColaboradores" class="listColab" style="display:none">
                 <div class="cont-header" id="cbcLista2">
                     <h1>Lista de colaboradores</h1>
