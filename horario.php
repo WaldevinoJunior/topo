8c8ee6f1eebd09d3cfeb3f41775856d2a2de5877
Gui20Henriques <guilherwme20@protonmail.com>
2023-01-31 11:48:58 -0300

    Aba de historico de presença no admin e afiliados na home

--- horario.php
+++ horario.php
@@ -56,6 +56,7 @@
                             <a href="./contratos.php">Contratos</a>
                             <a href="./pagamentos.php">Pagamentos</a>
                             <a href="./aniversariantes.php">Aniversariantes</a>
+                            <a href="./historico.php">Histórico de Presença</a>
                         </div>
             </li>
             <li style="cursor:pointer;color:rgb(216, 211, 211)"><i class="bi bi-gear"></i>Manutenção<i class="bi bi-caret-down"></i>
@@ -94,6 +95,7 @@
                             <a href="./contratos.php">Contratos</a>
                             <a href="./pagamentos.php">Pagamentos</a>
                             <a href="./aniversariantes.php">Aniversariantes</a>
+                            <a href="./historico.php">Histórico de Presença</a>
                         </div>
             </li>
             <li style="cursor:pointer;"><i class="bi bi-gear"></i>Manutenção<i class="bi bi-caret-down"></i>
