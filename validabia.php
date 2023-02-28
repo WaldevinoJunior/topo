d68296a2bd4c2fa3c974cead760148baa7dc1d1e
Gui20Henriques <guilherwme20@protonmail.com>
2023-02-28 16:17:54 -0300

    Update validabia.php

--- validabia.php
+++ validabia.php
@@ -816,8 +816,6 @@ if(isset($_POST['desconto'])){
     $cupons=$_POST['cupom']; 
     $id_Curso = $_POST['id'];
     $preco = $_POST['preco'];
-    $desc = 0.2;
-    $total = ((float)$preco) - (((float)$preco) * ($desc));
    // echo "<br> Total do Curso: ".$total."";
 //     echo "<br> Preço do Curso: ".$preco."";
   //   echo "<br> Desc do Curso: ".$desc."";
@@ -828,7 +826,8 @@ if(isset($_POST['desconto'])){
             $encontrou = true;
             echo "<br> QUANT: ".$linha[1]."";
              $hoje = date('Y-m-d');
-            
+            $desc = $linha[6];
+            $total = ((float)$preco) - (((float)$preco) * ($desc));
             if(($linha[1]!=0) && ($linha[2]>=$hoje)){
                 $linha[1]--;
                 echo $linha[1];
